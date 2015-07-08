0b6f5a4638c7594e85ea5fec5b6460629f117c46
lejacobo <jacobo@lanzarini.cl>
2015-07-08 20:26:29 -0300

    fixing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :productos
   
-  root 'product#index'
+  root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
